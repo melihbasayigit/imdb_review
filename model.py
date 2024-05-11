992a791a2e254e00c84be098bc9a04ac6ac70805
Melih Basayigit <melihbasayigit@gmail.com>
2024-05-11 18:43:51 +0300

    add two shuffle with seed in model

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import random
 
-seed_value = 78
+seed_value = 3131
+rating_amount = 280
 
 # CSV dosyasını oku
 df = pd.read_csv('rating_not_null.csv')
@@ -10,10 +11,23 @@ df = pd.read_csv('rating_not_null.csv')
 random.seed(seed_value)
 
 # Verileri karıştır
-df_shuffled = df.sample(frac=1, random_state=random.seed())
+df_shuffled = df.sample(frac=1, random_state=seed_value).reset_index(drop=True)
 
-print("İlk 100 veri:")
-print(df.head(100))
+# print("İlk 100 veri:")
+# print(df_shuffled.head(100))
 
-print("\nSon 100 veri:")
-print(df.tail(100))
+# print("\nSon 100 veri:")
+# print(df_shuffled.tail(100))
+
+sampled_indices = []
+for rating in range(1, 11):
+    indices = df_shuffled.index[df_shuffled['rating'] == rating].tolist()
+    sampled_indices.extend(indices[:rating_amount])
+
+sampled_df = df_shuffled.iloc[sampled_indices].reset_index(drop=True)
+
+sampled_df_shuffled = sampled_df.sample(frac=1, random_state=seed_value).reset_index(drop=True)
+
+print(sampled_df_shuffled)
+
+print(sampled_df_shuffled['rating'].value_counts())
